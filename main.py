83d8afd76bdf8f5896a36842c1106cb7983ae648
Sota6174 <t119141som110223@gmail.com>
2022-08-10 00:57:31 +0900

    加工画像のパスリストを変数に格納

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ os.makedirs(OUTPUT_IMAGE_DIR, exist_ok=True)
 
 if __name__ == "__main__":
     _, id, *path_list = sys.argv
-    print(id, path_list)
+    # print(id, path_list)
 
+    output_path_list = []
     for path in path_list:
         # image = remove_green_hsv(path)
         image = remove_green_chromakey(path)
 
         # 画像保存
         file_name = path.split("\\")[-1]
-        cv2.imwrite(f"{OUTPUT_IMAGE_DIR}/{file_name}", image)
+        output_path_list.append(f"{OUTPUT_IMAGE_DIR}/{file_name}")
+        cv2.imwrite(output_path_list[-1], image)
