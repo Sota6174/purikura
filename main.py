da20c0c0cc73de4406bd0c1e00d3efcb06cac092
MoAI522 <urotanre522@gmail.com>
2022-08-15 18:21:26 +0900

    パッケージ化

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
+from importlib.resources import path
 import os
 import sys
-from remove_gb import remove_green_hsv, remove_green_chromakey
+from typing import List
+from remove_gb import remove_green_chromakey
 import cv2
 import settings
 import tane_faceswap
 
-os.makedirs(settings.OUTPUT_IMAGE_DIR, exist_ok=True)
 
-if __name__ == "__main__":
-    _, id, *path_list = sys.argv
-    # print(id, path_list)
+def convert(path_list: List[str], template_id: int):
+    os.makedirs(settings.OUTPUT_IMAGE_DIR, exist_ok=True)
 
     output_path_list = []
     for path in path_list:
-        # image = remove_green_hsv(path)
         image = remove_green_chromakey(path)
 
-        if id == "0":
+        if template_id == "0":
             pass
-        elif id == "1":
+        elif template_id == "1":
             image = tane_faceswap.generate(image)
-        elif id == "2":
+        elif template_id == "2":
             pass
 
         # 画像保存
@@ -28,4 +27,10 @@ if __name__ == "__main__":
         output_path = os.path.join(settings.OUTPUT_IMAGE_DIR, file_name)
         cv2.imwrite(output_path, image)
         output_path_list.append(output_path)
-    print(output_path_list)
+    return output_path_list
+
+
+if __name__ == "__main__":
+    _, id, path_list = sys.argv
+    out_paths = convert(path_list=path_list, template_id=id)
+    print(out_paths)
